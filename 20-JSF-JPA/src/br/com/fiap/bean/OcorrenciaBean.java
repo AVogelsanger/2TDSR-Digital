5d46ad4b935df598b8d41c9125dabb8a606ef9b2
Logon Aluno <logonrmlocal@fiap.com.br>
2019-05-08 22:44:06 -0300

    Exerc 20

--- 20-JSF-JPA/src/br/com/fiap/bean/OcorrenciaBean.java
+++ 20-JSF-JPA/src/br/com/fiap/bean/OcorrenciaBean.java
@@ -7,7 +7,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 import br.com.fiap.dao.OcorrenciaDAO;
@@ -17,7 +17,7 @@ import br.com.fiap.exception.CommitException;
 import br.com.fiap.singleton.EntityManagerFactorySingleton;
 
 @ManagedBean
-@ViewScoped
+@RequestScoped
 public class OcorrenciaBean implements Serializable{
 
 	private Ocorrencia ocorrencia;
@@ -30,6 +30,28 @@ public class OcorrenciaBean implements Serializable{
 		dao = new OcorrenciaDAOImpl(EntityManagerFactorySingleton.getInstance().createEntityManager());
 	}
 	
+	/*validar data - metodo recriado no pacote validator na classe DataFuturaValidator
+	public void validaData(FacesContext context, UIComponent component, Object value)throws ValidatorException {
+		Date data = (Date) value;
+		
+		if (data.after(new Date())) {
+			throw new ValidatorException(new FacesMessage("Data inválida!"));
+		}
+	}*/
+	
+	
+	public String excluir(int codigo) {
+		try {
+			dao.remover(codigo);
+			dao.commit();
+			adicionarMensagem("Cadastro removido!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "lista-ocorrencia?faces-redirect=true";
+	}
+	
+	
 	public List<Ocorrencia> listar() {
 		return dao.listar();
 	}
@@ -37,9 +59,14 @@ public class OcorrenciaBean implements Serializable{
 	public String cadastrar() {
 		
 		try {
-			dao.cadastrar(ocorrencia);
+			if (ocorrencia.getCodigo() == 0) {
+				dao.cadastrar(ocorrencia);
+				adicionarMensagem("Cadastrado com sucesso!");
+			}else {
+				dao.alterar(ocorrencia);
+				adicionarMensagem("Atualizado com sucesso!");
+			}
 			dao.commit();
-			adicionarMensagem("Cadastrado com sucesso!");
 			return "listar-ocorrencia?faces-redirect=true";
 		} catch (CommitException e) {
 			e.printStackTrace();
