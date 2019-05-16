46798f218f9275c9f09cd584612e9a127e10d579
Logon Aluno <logonrmlocal@fiap.com.br>
2019-05-15 22:44:47 -0300

    20 e 21 JSF

---
+++ _pasted_code_/src/snippet/Snippet.java
@@ -0,0 +1,13 @@
+package snippet;
+
+public class Snippet {
+	titulo_pagina = Occurrence Record
+	link_lista = List
+	lbl_data = Data
+	lbl_artigo = article
+	lbl_anonimo = Anonimous
+	lbl_descricao = Description
+	lbl_denunciante = squeaker
+	btn_cadastrar = Register
+}
+
